3e5a0a27fbe479a4c240e39cba8bfa3f69469bc1
Brendan Lawless <brendanlawless@hotmail.com>
2024-02-10 18:32:14 +0000

    API Route Handlers

--- app/components/LatestTodos.tsx
+++ app/components/LatestTodos.tsx
@@ -1,15 +1,22 @@
 'use client'
+
 import { useSession } from "next-auth/react"
+import { useEffect, useState } from 'react'
 
 export default function LatestTodos() {
 
     const session = useSession()
+    const [message, setMessage] = useState(null)
 
-    console.log(session)
+    useEffect(() => {
+        fetch('http://localhost:3000/api/todos')
+            .then(res => res.json())
+            .then(data => setMessage(data.message))
+    }, [message])
 
     return (
         <ul>
-
+            <li>{message}</li>
         </ul>
     )
 }
