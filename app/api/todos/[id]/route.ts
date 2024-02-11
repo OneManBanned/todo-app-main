ed293125fb78e0e8dd24f35aee43a1c05d01d76f
Brendan Lawless <brendanlawless@hotmail.com>
2024-02-11 20:57:33 +0000

    get route hanlder

---
+++ app/api/todos/[id]/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import dbConnect from "@/app/lib/dbConnect";
+import User from "@/app/lib/userModel"
+import Todo from "@/app/lib/todoModel"
+
+export const dynamic = 'force-dynamic'
+
+export async function GET(req: NextRequest, { params }: { params: { id: string }}) {
+
+    console.log("Called in GET", params )
+
+/*
+    dbConnect()
+
+    try {
+
+        const populatedUser = await User
+            .findById(params.id)
+            .populate({path: "todos", model: Todo})
+            .select("todos")
+
+        console.log(populatedUser)
+        
+    } catch(e) {
+
+        console.log(e)
+    }
+
+   */
+
+    return NextResponse.json({"message": "hello"})
+
+}
--- app/components/LatestTodos.tsx
+++ app/components/LatestTodos.tsx
@@ -6,17 +6,23 @@ import { useEffect, useState } from 'react'
 export default function LatestTodos() {
 
     const session = useSession()
-    const [message, setMessage] = useState(null)
+    const [message, setMessage] = useState([])
+    let idObj = session.data?.user.id
 
-    useEffect(() => {
-        fetch('http://localhost:3000/api/todos')
-            .then(res => res.json())
-            .then(data => setMessage(data.message))
-    }, [message])
+    if (session.status == 'authenticated') {
+
+        useEffect(() => {
+             fetch(`http://localhost:3000/api/todos/${idObj}`)
+                .then(res => res.json())
+                .then(d => console.log("DATA", d))
+        }, [message])
+
+
+    }
 
     return (
         <ul>
-            <li>{message}</li>
+            <li></li>
         </ul>
     )
 }
