28508e7734ee8724fee87b9595c162241e94a758
Brendan Lawless <brendanlawless@hotmail.com>
2024-02-28 18:53:59 +0000

    commented out custom login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,29 @@
-import { useFormState, useFormStatus } from "react-dom"
-import { authenticate } from "../lib/actions"
+
+/*
+'use client'
+
+import { useFormState } from "react-dom"
 
 const Login = () => {
+
     const [errorMessage, dispatch] = useFormState(authenticate, undefined)
+
     return (
 
-<form action={dispatch}>
-<label htmlFor="name">
-<input type="text" id="name"/>
-</label>
-<div>
-{errorMessage && (
-<>
-<p>{errorMessage}</p>
-</>
-)}
-</div>
-</form>
+        <form action={dispatch}>
+            <label htmlFor="name">
+                <input type="text" id="name" />
+            </label>
+            <div>
+                {errorMessage && (
+                    <>
+                        <p>{errorMessage}</p>
+                    </>
+                )}
+            </div>
+        </form>
     )
 }
 
 export default Login
+*/
