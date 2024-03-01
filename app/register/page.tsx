57d161bbf4a825dd64a4f3a9d937c327742b109d
Brendan Lawless <brendanlawless@hotmail.com>
2024-03-01 19:30:41 +0000

    added register path

---
+++ app/register/page.tsx
@@ -0,0 +1,20 @@
+@@ -1,19 +0,0 @@
+'use client'
+
+import { registerUser } from "../lib/user_actions"
+
+
+export default async function Register() {
+
+    return (
+        <form action={registerUser} className="flex flex-col gap-2 mx-auto max-w-md mt-10">
+            <label htmlFor="name">Enter username</label>
+            <input className="border border-black" type="name" name="name" id="name" />
+            <label htmlFor="password">Enter password</label>
+            <input className="border border-black" type="password" name="password" id="password" />
+            <label htmlFor="passwordCheck">Re-enter password</label>
+            <input className="border border-black" type="password" name="passwordCheck" id="passwordCheck" />
+            <button type="submit">Register</button>
+        </form>
+    )
+}
