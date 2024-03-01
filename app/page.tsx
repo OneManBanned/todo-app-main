d7369e5e9a4273f963a1ac7434b1fbe8e32651c4
Brendan Lawless <brendanlawless@hotmail.com>
2024-03-01 15:45:59 +0000

    edited .env

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
     if (session) {
 
         const fetchDatabaseTodos = await fetch(
-            `${process.env.NEXT_PUBLIC_URL}/api/todos/${session.user?.id}`,
+            `${process.env.VERCEL_URL}/api/todos/${session.user?.id}`,
             { next: { tags: ['todos'] } }
         )
 
