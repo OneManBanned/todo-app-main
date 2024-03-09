28d9f6653e80da66c910439958604726df0b14a2
Brendan Lawless <brendanlawless@hotmail.com>
2024-03-09 18:09:33 +0000

    register

--- app/lib/user_actions.ts
+++ app/lib/user_actions.ts
@@ -4,6 +4,7 @@ import dbConnect from "../lib/dbConnect";
 import { z } from 'zod';
 import User from '@/app/lib/userModel'
 import bcrypt from 'bcrypt'
+import { revalidatePath } from "next/cache";
 import { redirect } from 'next/navigation'
 
 const FormSchema = z.object({
@@ -73,6 +74,7 @@ export async function registerUser(prevState: State, formData: FormData) {
 
     }
 
+    revalidatePath('/', 'page')
     redirect('api/auth/signin')
 
 }
